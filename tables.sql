64022830a72cd0ab055c4e881fe01520d682ea9f
Yichang Liu <39092148+PoisonWater@users.noreply.github.com>
2020-05-02 11:09:23 +0800

    Change Formatting

--- tables.sql
+++ tables.sql
@@ -12,7 +12,7 @@ USE `databaseproj`;
 
 -- Create Relation Person
 CREATE TABLE User (
-	User_id int(11) NOT NULL AUTO_INCREMENT,
+	User_id int(11) NOT NULL,
     Person_Name varchar(40) NOT NULL,
     Phone_number varchar(11) DEFAULT NULL,
     Person_Password varchar(30) NOT NULL,
@@ -89,68 +89,56 @@ CREATE TABLE Follow(
     FOREIGN KEY (Follower_id) REFERENCES User(User_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `category` (
-  `categoryID` int(11) NOT NULL,
-  `categoryName` varchar(40) NOT NULL,
-  `categoryDesc` varchar(150) DEFAULT NULL
+
+
+
+CREATE TABLE Category (
+	Category_id int(11) NOT NULL,
+    Category_name varchar(40) NOT NULL,
+    Category_desc varchar(128) DEFAULT NULL,
+    PRIMARY KEY (Category_id)
+)ENGINE=InnoDB;
+
+CREATE TABLE Vendor (
+  User_id int(11) NOT NULL,
+    vname varchar(40) NOT NULL,
+    vaddress varchar(150) DEFAULT NULL,
+    vphoneNo char(10) DEFAULT NULL,
+    vservice varchar(128) DEFAULT NULL,
+    venueOpenTime time DEFAULT NULL,
+    venueCloseTime time DEFAULT NULL,
+    PRIMARY KEY (User_id),
+    FOREIGN KEY (User_id) REFERENCES User(User_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `vendor_category` (
-  `vendorID` int(11) NOT NULL,
-  `categoryID` int(11) NOT NULL
+CREATE TABLE Vendor_category (
+  User_id int(11) NOT NULL,
+  Category_id int(11) NOT NULL,
+  PRIMARY KEY (User_id, Category_id),
+  FOREIGN KEY (User_id) REFERENCES Vendor(User_id),
+  FOREIGN KEY (Category_id) REFERENCES Category(Category_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `preference_vendor` (
-  `userID` int(11) NOT NULL,
-  `vendorID` int(11) NOT NULL
+CREATE TABLE Preference_vendor (
+  Customer_id int(11) NOT NULL,
+  Vendor_id int(11) NOT NULL,
+  PRIMARY KEY (Customer_id, Vendor_id),
+  FOREIGN KEY (Customer_id) REFERENCES Customer(User_id),
+  FOREIGN KEY (Vendor_id) REFERENCES Vendor(User_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `vendor` (
-  `vendorID` int(11) NOT NULL,
-  `name` varchar(40) NOT NULL,
-  `address` varchar(150) DEFAULT NULL,
-  `phoneNo` char(10) DEFAULT NULL,
-  `service` varchar(128) DEFAULT NULL,
-  `venueOpenTime` time DEFAULT NULL,
-  `venueCloseTime` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO Vendor (Vendor_id, vname, vaddress, vphoneNo, vservice, venueOpenTime, venueCloseTime) VALUES
+(1001, 'Pandora Plaza', 'Lower Campus - Student\'s Activity Bldg. 1F', '3122660302', 'High Quality Food Supply', '10:00:00', '23:00:00'),
+(1002, 'LeTian #1 Cafeteria', 'Lower Campus - LeTian Bldg. 1F', '3122661240', 'Food Supply with Reasonable Price', '10:30:00', '21:00:00');
 
-INSERT INTO `vendor` (`vendorID`, `name`, `address`, `phoneNo`, `service`, `venueOpenTime`, `venueCloseTime`) VALUES
-(1, 'Pandora Plaza', 'Lower Campus - Student\'s Activity Bldg. 1F', '3122660302', 'High Quality Food Supply', '10:00:00', '23:00:00'),
-(2, 'LeTian #1 Cafeteria', 'Lower Campus - LeTian Bldg. 1F', '3122661240', 'Food Supply with Reasonable Price', '10:30:00', '21:00:00');
-
-INSERT INTO `category` (`categoryID`, `categoryName`, `categoryDesc`) VALUES
+INSERT INTO Category (Category_id, Category_name, Category_desc) VALUES
 (1, 'Consult', 'Offering Consulting Services'),
 (2, 'Food Supply', 'Providing food supply');
 
-INSERT INTO `preference_vendor` (`userID`, `vendorID`) VALUES
-(1, 1),
-(2, 2);
+INSERT INTO `preference_vendor` (Customer_id, Vendor_id) VALUES
+(1, 1001),
+(2, 1002);
 
-INSERT INTO `vendor_category` (`vendorID`, `categoryID`) VALUES
-(1, 1),
-(2, 2);
-
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`categoryID`);
-
-ALTER TABLE `vendor_category`
-  ADD PRIMARY KEY (`vendorID`,`categoryID`),
-  ADD KEY `categoryID` (`categoryID`);
-
-ALTER TABLE `preference_vendor`
-  ADD PRIMARY KEY (`userID`, `vendorID`),
-  ADD KEY `vendorID` (`vendorID`);
-
-ALTER TABLE `vendor`
-  ADD PRIMARY KEY (`vendorId`);
-
-ALTER TABLE `category`
-  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `vendor`
-  MODIFY `vendorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
-ALTER TABLE `vendor_category`
-  ADD CONSTRAINT `cat_vendor_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `category` (`categoryID`),
-  ADD CONSTRAINT `cat_vendor_ibfk_2` FOREIGN KEY (`vendorID`) REFERENCES `vendor` (`vendorID`);
+INSERT INTO Vendor_category (User_id, Category_id) VALUES
+(1001, 1),
+(1002, 2);
